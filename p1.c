5f22a0568f0e4ee0dcbdb18f1334b4790855a98e
abid42214 <abidhossan566@gmail.com>
2025-09-30 10:46:03 +0600

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main (){
+int a,square,cube;
+printf("enter the value of a :");
+scanf("%d",&a);
+square=a*a;
+printf("The ans is:%d \n",square);
+cube=a*a*a;
+printf("The ans is:%d",cube);
+
+
+return 0;
+}
